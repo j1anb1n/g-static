5672f51fb19fd4650a1b98cb436c9a940cea8b7e
陈建镔 <chenjianbin@ganji.com>
2014-06-08 17:43:46 +0800

    add wiki/detail

--- src/app/haorourou/page/base.js
+++ src/app/haorourou/page/base.js
@@ -1,7 +1,10 @@
 var $ = require('$');
+var Holder = require('com/desktop/lib/holder/holder.js');
 
 exports.init = function () {
     if (window.screen.width > 1280) {
         $('body').addClass('lg-screen');
     }
+
+    Holder.run();
 };
